db0dfa3353b0989af689caf3d003a6ce21654a0f
letam2809 <tamle289992@gmail.com>
2024-07-21 02:09:59 +0700

    Thêm File Student.java Video 26.java

---
+++ src/Package04/Student.java
@@ -0,0 +1,22 @@
+package Package04;
+
+public class Student {
+private String name;
+private int age;
+
+    public Student() {
+
+    }
+
+    public Student (String name, int age){ 
+        this.name = name; 
+        this.age = age;
+    }    
+    public String getName() {
+        return this.name;
+    }
+    public int getAge(){
+        return this.age;
+    }
+
+}
